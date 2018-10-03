c4a7cd1b3d9c12a288fcb2478dfc0855dd4d8f91
daniels <daniels.uth702@gmail.com>
2018-10-03 15:54:41 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -34,8 +34,10 @@ router.beforeEach((to, from, next) => {
     store.dispatch('saveToken', { token: cookieMonster })
     store.dispatch('fetchUserInfo', { token: cookieMonster }).then(() => {
       if (store.state.users.responseData.message === 'Token has expired') {
+        console.log(store.state.users)
         store.dispatch('getUserNewToken', { token: cookieMonster }).then(() => {
-          console.log(store.state)
+          console.log(store.state.users)
+          console.log('flag')
           // The boooody
           var tk = jwtHeader + 'eyJ' + store.state.users.responseData.token.slice(3)
           // Surprise them all
