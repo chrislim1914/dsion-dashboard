cfbf793388261795aba7f5e7bffc3bb087b0d04c
daniels <daniels.uth702@gmail.com>
2018-10-03 17:01:00 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -38,10 +38,10 @@ router.beforeEach((to, from, next) => {
           if (store.state.users.responseData === 'token_invalid' || store.state.users.responseData.result === false) {
             alert('Please login again.')
             // Expire cookies
-            $cookie.delete('tka', {domain: '.dsion.io'})
-            $cookie.delete('tkp', {domain: '.dsion.io'})
-            $cookie.delete('tks', {domain: '.dsion.io'})
-            $cookie.delete('b', {domain: '.dsion.io'})
+            VueCookie.delete('tka', {domain: '.dsion.io'})
+            VueCookie.delete('tkp', {domain: '.dsion.io'})
+            VueCookie.delete('tks', {domain: '.dsion.io'})
+            VueCookie.delete('b', {domain: '.dsion.io'})
             // window.location.href = 'http://localhost:8081'
             window.location.href = 'https://dsion.io'
           } else {
@@ -51,19 +51,19 @@ router.beforeEach((to, from, next) => {
             var surprise = pickANumber()
             // Shifting shifters to shifted
             var shifted = shifters(tk, surprise).split('.')
-            $cookie.set('tka', shifted[0], {expires: 1, domain: '.dsion.io'})
-            $cookie.set('tkp', shifted[1], {expires: 1, domain: '.dsion.io'})
-            $cookie.set('tks', shifted[2], {expires: 1, domain: '.dsion.io'})
-            $cookie.set('b', surprise, {expires: 1, domain: '.dsion.io'})
+            VueCookie.set('tka', shifted[0], {expires: 1, domain: '.dsion.io'})
+            VueCookie.set('tkp', shifted[1], {expires: 1, domain: '.dsion.io'})
+            VueCookie.set('tks', shifted[2], {expires: 1, domain: '.dsion.io'})
+            VueCookie.set('b', surprise, {expires: 1, domain: '.dsion.io'})
           }
         })
       } else if (store.state.users.responseData === 'token_invalid') {
         alert('Please login again.')
         // Expire cookies
-        $cookie.delete('tka', {domain: '.dsion.io'})
-        $cookie.delete('tkp', {domain: '.dsion.io'})
-        $cookie.delete('tks', {domain: '.dsion.io'})
-        $cookie.delete('b', {domain: '.dsion.io'})
+        VueCookie.delete('tka', {domain: '.dsion.io'})
+        VueCookie.delete('tkp', {domain: '.dsion.io'})
+        VueCookie.delete('tks', {domain: '.dsion.io'})
+        VueCookie.delete('b', {domain: '.dsion.io'})
         // window.location.href = 'http://localhost:8081'
         window.location.href = 'https://dsion.io'
       }
