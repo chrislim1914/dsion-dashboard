77b3e8697022644662366252b087b886138a13e3
daniels <daniels.uth702@gmail.com>
2018-12-06 10:38:00 +0800

    Added country code json

--- src/components/KycApplication.vue
+++ src/components/KycApplication.vue
@@ -66,12 +66,18 @@
                               Phone Number
                               <span :class="{'d-none': locale !== 'kr'}">(전화번호)</span>
                             </label>
-                            <select v-model="kycForm.countryCode" class="input-bordered" name="country-code" id="countrcode">
-                              <option :value="countrycode.dial_code" :key="index" v-for="(countrycode, index) in CountryCodes">
-                                {{ countrycode.dial_code }} {{ countrycode.dial_code }}
-                              </option>
-                            </select>
-                            <input v-model="kycForm.phoneNumber" class="input-bordered" type="text" id="phone-number" name="phone-number" required>
+                            <div class="row">
+                              <div class="col-4" style="padding-right: 5px;">
+                                <select v-model="kycForm.countryCode" class="input-bordered" name="country-code" id="countrycode">
+                                  <option :value="countrycode.dial_code" :key="index" v-for="(countrycode, index) in countrycodes">
+                                    {{ countrycode.dial_code }} {{ countrycode.dial_code }}
+                                  </option>
+                                </select>
+                              </div>
+                              <div class="col-8" style="padding-left: 0px !important; margin-left: 0px;">
+                                <input v-model="kycForm.phoneNumber" class="input-bordered" type="text" id="phone-number" name="phone-number" required>
+                              </div>
+                            </div>
                           </div>
                           <!-- .input-item -->
                         </div>
@@ -357,6 +363,7 @@ export default {
       isLoading: false,
       locale: '',
       nationalities: Nationalities,
+      countrycodes: CountryCodes,
       kycForm: {
         firstName: '',
         lastName: '',
