957910aebf968c50a46d4763e68dc9d911e7c4c1
daniels <daniels.uth702@gmail.com>
2018-12-17 15:58:57 +0800

    Added chinese language

--- src/localization/index.js
+++ src/localization/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import english from './english.json'
 import korean from './korean.json'
+import chinese from './chinese.json'
 
 Vue.use(VueI18n)
 
 const messages = {}
 messages.en = english
 messages.kr = korean
+messages.ch = chinese
 
 const i18n = new VueI18n({
   locale: 'kr',
